6eb0b1a26c34c075c05e4ad538f87af2df779166
Maxim Nedobezhkin <m.nedobezhkin@astondevs.ru>
2025-05-03 23:57:14 +0700

    init commit

---
+++ src/main/java/com/alpengotter/dental_service_bank/DentalServiceBankApplication.java
@@ -0,0 +1,13 @@
+package com.alpengotter.dental_service_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalServiceBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalServiceBankApplication.class, args);
+    }
+
+}
