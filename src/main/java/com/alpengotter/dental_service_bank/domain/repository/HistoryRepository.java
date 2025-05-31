674b26b37f2f15a479e99c90f7987c4bfc73b35a
Maxim Nedobezhkin <m.nedobezhkin@astondevs.ru>
2025-05-31 19:03:28 +0700

    правка истории для фильтрации по клиникам

--- src/main/java/com/alpengotter/dental_service_bank/domain/repository/HistoryRepository.java
+++ src/main/java/com/alpengotter/dental_service_bank/domain/repository/HistoryRepository.java
@@ -32,12 +32,14 @@ public interface HistoryRepository extends JpaRepository<HistoryEntity, Integer>
     @Query("select h from HistoryEntity h "
         + "left join UserEntity u "
         + "on u.id = h.user.id "
+        + "left join UserClinicMapEntity ucm "
+        + "on ucm.user.id = h.user.id "
         + "where (cast(h.date as date) between cast(:dateFrom as date) and cast(:dateTo as date)) "
         + "and ((lower(u.firstName) like lower(concat('%', :firstName, '%'))) "
         + "or (lower(u.lastName) like lower(concat('%', :lastName, '%'))) "
         + "or (lower(u.surname) like lower(concat('%', :surname, '%'))) "
         + "or (lower(h.comment) like lower(concat('%', :comment, '%')))) "
-        + "and (h.clinic.id in :clinicIds) "
+        + "and (:clinicIds is null or ucm.clinic.id in :clinicIds) "
         + "order by h.id desc ")
     Set<HistoryEntity> findAllByDateBetweenAndUserFirstNameContainingOrUserLastNameContainingOrderByIdDesc(
         LocalDateTime dateFrom,
