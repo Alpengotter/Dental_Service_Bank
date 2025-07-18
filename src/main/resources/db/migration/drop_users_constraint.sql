99ce59e901c4849c8fafca8458ef1765e7545b1f
Maxim Nedobezhkin <m.nedobezhkin@astondevs.ru>
2025-07-18 21:59:41 +0700

    удалил not null

--- src/main/resources/db/migration/drop_users_constraint.sql
+++ src/main/resources/db/migration/drop_users_constraint.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset Nedobezhkin.M.I.:drop_user_constraint
-ALTER TABLE user ALTER COLUMN first_name DROP NOT NULL;
+ALTER TABLE users ALTER COLUMN first_name DROP NOT NULL;
 --rollback ;
